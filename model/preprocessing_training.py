cdbbd7785bacf3d5976beb8698018c10dfef629f
Zender-Aurelien <zenderaurelien4@gmail.com>
2025-08-08 00:21:46 -0400

    Update preprocessing_training.py

--- model/preprocessing_training.py
+++ model/preprocessing_training.py
@@ -1,6 +1,5 @@
 import pandas as pd #import necessary libraries
 from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
